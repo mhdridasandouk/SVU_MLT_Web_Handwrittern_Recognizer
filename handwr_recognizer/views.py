9c4bce6d0764e5b786695a88211d94244a7a814c
Mohammad Rida Sandouk <mhdridasandouk00@gmail.com>
2026-06-26 22:06:00 +0300

    fix frontend paths

--- handwr_recognizer/views.py
+++ handwr_recognizer/views.py
@@ -3,7 +3,7 @@ from django.apps import apps
 
 def recognize_digit(request):
     """view function that renders the upload.htlm page where the actual image processing occures"""
-    return render(request, 'handwr_recognizer/upload.html')
+    return render(request, 'handwr_recognizer/handwr_recognizer/upload.html')
 def home(request):
     """view function that renders the home intro page"""
-    return render(request, 'handwr_recognizer/home.html')
+    return render(request, 'handwr_recognizer/handwr_recognizer/home.html')
